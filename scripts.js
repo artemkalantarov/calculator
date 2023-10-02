3639ed0dd08c878b839e89f759bc75e930f767fd
Art <artem1324@gmail.com>
2023-10-01 23:58:16 -0700

    Set up initial look of the app and some functions
    This sets up the visuals of the project. You now have a calculator with
    buttons. This is the initial design. There is now also an operate
    function which decides what to do with numbers based on the operator it
    receives.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 };
+
+function operate(num1, num2, operator){
+    if (operator === '+'){
+        return add(num1,num2);
+    } else if (operator === '-'){
+        return subtract(num1,num2);
+    } else if (operator === '*'){
+        return multiply(num1,num2);
+    } else if (operator === '/'){
+        return divide(num1,num2);
+    };
+};
+
+let num1 = 0 
+let num2 = 0
+let operator = ''
